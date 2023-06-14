f602208f6d2f602bec42d075e9ed60c3a13ebc80
soclarence <clarenceaigbuza@gmail.com>
2023-06-14 20:55:20 +0100

    updated footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,6 +61,28 @@ export default function Home() {
 
         <Hero />
         <Work />
+
+        <div className="px-5 lg:px-[4.5rem] text-sm pt-20 flex flex-wrap items-start lg:space-x-10 lg:space-y-0 space-y-10">
+
+          <p className="text-white lg:w-[35%]">I have a passion project I work on on the side called <span className="font-bold hover:underline"><a href="https://innvesio.com" rel="noopener" target="_blank">INNVESIO,</a></span> it's a think tank for me and a few exceptional people to play around with tech ideas, learn and have fun while doing it. <br/>I also help my friends at Heracules design and build amazing products.</p>
+
+          <div>
+            <h1 className="text-white mb-5 text-[1.5rem]">BOOKMARKS</h1>
+            
+            <section className="space-y-10">
+              <div className="lg:pr-[50%]">
+                <p className="text-white font-semibold">CAMELOT UI — DESIGN STSYEM (UI Kit)</p>
+                <p className="text-gray-400">A little design system and components I worked on for a social commerce platform</p>
+              </div>
+
+              <div className="lg:pr-[50%]">
+                <p className="text-white font-semibold">YOURPADI</p>
+                <p className="text-gray-400">Play around with one of my Live projects</p>
+              </div>
+            </section>
+          </div>
+        </div>
+
         <Footer />
     </div>
   )
